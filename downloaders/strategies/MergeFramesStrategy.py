8db3408f95f25e4890e088b58bd44cef99a3b5b5
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-09-12 09:47:19 +0200

    Add height limit for MergeFramesStrategy.py.

--- downloaders/strategies/MergeFramesStrategy.py
+++ downloaders/strategies/MergeFramesStrategy.py
@@ -5,8 +5,23 @@ import os
 
 class MergeFramesStrategy(DownloadStrategy):
 
-    def __init__(self, save_original: bool = False):
+    def __init__(self, save_original: bool = False, max_size: int = 25000):
         self.__save_original: bool = save_original
+        self.__max_height: int = max_size
+
+    @staticmethod
+    def MergeFiles(files: Image.Image, path: str):
+        global_width = max(files, key=lambda i: i.width).width
+        total_height = sum(img.height for img in files)
+
+        result = Image.new("RGBA", (global_width, total_height))
+        y_offset = 0
+        for img in files:
+            current_x_pos = (global_width - img.width) // 2
+
+            result.paste(img, (current_x_pos, y_offset))
+            y_offset += img.height
+        result.save(path)
 
     def Execute(self, path: str):
 
@@ -16,20 +31,21 @@ class MergeFramesStrategy(DownloadStrategy):
         if not files:
             return
 
+        sort_images = []
+
         images = [Image.open(f) for f in files]
 
-        global_width = max(images, key=lambda i: i.width).width
-        total_height = sum(img.height for img in images)
+        rest_height = 0
+        for image in images:
+            if image.height > rest_height:
+                sort_images.append([image])
+                rest_height = self.__max_height - image.height
+            else:
+                rest_height -= image.height
+                sort_images[-1].append(image)
 
-        result = Image.new("RGBA", (global_width, total_height))
-        y_offset = 0
-        for img in images:
-
-            current_x_pos = (global_width - img.width) // 2
-
-            result.paste(img, (current_x_pos, y_offset))
-            y_offset += img.height
-        result.save(os.path.join(path, "merged.png"))
+        for i, sort_image_list in enumerate(sort_images):
+            self.MergeFiles(sort_image_list, os.path.join(path, f"merge_{i}.png"))
 
         if not self.__save_original:
             for f in files:
