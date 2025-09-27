415f136aedafc098b598bf4d9867a8ad354e3181
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-09-27 22:32:32 +0200

    Change SavePDFStrategy.py. Now saving all frames in one pdf file.

--- downloaders/strategies/SavePDFStrategy.py
+++ downloaders/strategies/SavePDFStrategy.py
@@ -11,28 +11,39 @@ class SavePDFStrategy(DownloadStrategy):
 
     def Execute(self, path: str):
 
-        files = os.listdir(path)
+        name = os.path.basename(path)
+
+        output_path = os.path.join(path, f"{name}.pdf")
+
+        saving_images = []
+        tmp_images = []
+
+        print(f"Saving PDF ({name})")
+
+        files = [file for file in os.listdir(path) if file.lower().endswith((".jpg", ".png"))]
+        files.sort(
+            key=lambda name: int(name.replace(".jpg", "").replace(".png", "")),
+        )
+
         for file in files:
-            if file.lower().endswith((".jpg", ".png")):
-                full_path = os.path.join(path, file)
+            full_path = os.path.join(path, file)
 
-                tmp_path = None
-                with Image.open(full_path) as image:
-                    if image.mode in ("RGBA", "LA"):
-                        background = Image.new("RGB", image.size, (255, 255, 255))
-                        background.paste(image, mask=image.getchannel("A"))
-                        image = background
-                        tmp_path = os.path.splitext(full_path)[0] + "_noalpha.jpg"
-                        image.save(tmp_path, "JPEG")
-                        full_path = tmp_path
+            with Image.open(full_path) as image:
+                if image.mode in ("RGBA", "LA"):
+                    background = Image.new("RGB", image.size, (255, 255, 255))
+                    background.paste(image, mask=image.getchannel("A"))
+                    image = background
+                    tmp_path = os.path.splitext(full_path)[0] + "_noalpha.jpg"
+                    image.save(tmp_path, "JPEG", dpi=(300, 300))
+                    full_path = tmp_path
 
-                pdf_bytes = img2pdf.convert(full_path)
+                    tmp_images.append(tmp_path)
 
-                file_name = os.path.splitext(file)[0]
-                output_path = os.path.join(path, f"{file_name}.pdf")
+            saving_images.append(full_path)
 
-                if tmp_path is not None:
-                    os.remove(tmp_path)
+        with open(output_path, "wb") as f:
+            data = img2pdf.convert(saving_images)
+            f.write(data)
 
-                with open(output_path, "wb") as f:
-                    f.write(pdf_bytes)
+        for tmp_image in tmp_images:
+            os.remove(tmp_image)
