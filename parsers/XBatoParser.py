8fceb835365ad6c560775a11ebe78650e5336a31
NikitaVoliaPC <volanskijnikita2@gmail.com>
2025-10-12 10:08:07 +0200

    Fixe XBatoParser.py to new site version.

--- parsers/XBatoParser.py
+++ parsers/XBatoParser.py
@@ -12,7 +12,7 @@ class XBatoParser(MangaParserInterface):
     def ParseTitle(content: str) -> str | None:
 
         soup = BeautifulSoup(content, "html.parser")
-        titleWrapper = soup.find("h3", {"class": "item-title"})
+        titleWrapper = soup.find("h3", {"class": "text-lg"})
         if titleWrapper is None:
             return None
 
@@ -25,46 +25,45 @@ class XBatoParser(MangaParserInterface):
 
     @staticmethod
     def ParsePagesLinks(content: str) -> list[str] | None:
-
-        imagesListe = re.findall(r"const imgHttps = \[(\"[\w\W\d/:]*\")*]", content)
-        if len(imagesListe) == 0:
-            return None
-        pages = imagesListe[0].replace('"', '').split(",")
-        return pages
+        rep = []
+        mangaPages = re.findall(
+            r"https://[^\s\"']+/media/[^\s\"']+\.(?:jpg|jpeg|png|gif|webp)",
+            content,
+            re.IGNORECASE,
+        )
+        for item in mangaPages:
+            rep += item.replace("\\\\&quot;", "").replace("[0,", "").split("],")
+        return rep
 
     @staticmethod
     def ParsePosterLink(content: str) -> str | None:
         soup = BeautifulSoup(content, "html.parser")
-        posterWrapper = soup.find("div", {"class": "attr-cover"})
-        if posterWrapper is None:
-            return None
-
-        poster = posterWrapper.find("img")
+        poster = soup.find("img", {"class": "w-full not-prose shadow-md shadow-black/50"})
         return poster.get("src") if not poster is None else None
 
     @staticmethod
     def ParseChaptersLinks(content: str) -> list[str] | None:
         soup = BeautifulSoup(content, "html.parser")
 
-        chaptersList = soup.find("div", {"class": "episode-list"})
+        chaptersList = soup.find_all("astro-slot")
         if chaptersList is None:
             return None
 
-        chapters = chaptersList.find_all("a", {"class": "chapt"})
+        chapters = chaptersList[1].find_all("a", {"class": "visited:text-accent"})
 
-        return ["https://xbato.com" + chapter.get("href") for chapter in chapters]
+        return ["https://xbato.com" + chapter.get("href") + "?=load=2" for chapter in chapters]
 
     @staticmethod
     def ParseChapters(content: str) -> list[MangaChapter] | None:
 
         soup = BeautifulSoup(content, "html.parser")
 
-        chaptersList = soup.find("div", {"class": "episode-list"})
+        chaptersList = soup.find_all("astro-slot")
         if chaptersList is None:
             return None
 
-        chapters = chaptersList.find_all("a", {"class": "chapt"})
+        chapters = chaptersList[1].find_all("a", {"class": "visited:text-accent"})
 
         textFilter = lambda s: s.replace("  ", "").replace("\\n", "")
 
-        return [MangaChapter("https://xbato.com" + chapter.get("href"), textFilter(chapter.text)) for chapter in chapters]
+        return [MangaChapter("https://xbato.com" + chapter.get("href") + "?=load=2", textFilter(chapter.text)) for chapter in chapters]
